0e9309e73a6645157a64b8598a3817a04e3003a3
Awesome65432139 <94124691+Awesome65432139@users.noreply.github.com>
2023-10-17 12:25:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -38,6 +38,23 @@ function checkPswd() {
           frozen = false;
       }
 }
+
+  function openGamel () {
+      if (frozen == false) {
+        var win = window.open()
+        var url = "https://awesome65432139.github.io/math/"
+        var iframe = win.document.createElement('iframe')
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.border = "none";
+        iframe.src = url
+        win.document.body.appendChild(iframe)
+        window.location.replace('https://ironx-studios.github.io/gxmes/');
+      } else {
+          window.location.replace('https://ironx-studios.github.io/gxmes/');
+          frozen = false;
+      }
+}
 // Try later to create panic button
   function panic () {
     window.location.replace('https://portal.edinaschools.org/Home/Login?ReturnUrl=%2fSSO');
